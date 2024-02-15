31fe5759cf380b99f59e00a1dd11f729bab3ece6
Mohan Reddy Rondla <157732383+MohanreddyRondla@users.noreply.github.com>
2024-02-15 23:09:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var randomdiceimage="./images/dice"+ randomNumber1 +".png";
 var img1 =document.querySelectorAll("img")[0];
 img1.setAttribute("src", randomdiceimage);
 var randomNumber2= Math.floor(Math.random()*6)+1;
-var randomdiceimage2="./images/dice"+ randomNumber2 +".png";
+var randomdiceimage2="./dice"+ randomNumber2 +".png";
 var img2 =document.querySelectorAll("img")[1];
 img2.setAttribute("src", randomdiceimage2);
 if(randomNumber1 > randomNumber2){
