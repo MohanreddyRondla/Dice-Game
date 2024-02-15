bfd941fc1cc3e19d6d12577062da691489af90ec
Mohan Reddy Rondla <157732383+MohanreddyRondla@users.noreply.github.com>
2024-02-15 23:14:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var randomNumber1= Math.floor(Math.random()*6)+1;
-var randomdiceimage="./images/dice"+ randomNumber1 +".png";
+var randomdiceimage="./dice"+ randomNumber1 +".png";
 var img1 =document.querySelectorAll("img")[0];
 img1.setAttribute("src", randomdiceimage);
 var randomNumber2= Math.floor(Math.random()*6)+1;
